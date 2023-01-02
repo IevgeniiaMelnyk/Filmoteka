73b1ff5f9bec4ac1862612bb4569b6b9aaba9bd4
Ievgeniia Melnyk <agnikava@gmail.com>
2023-01-03 00:35:05 +0200

    add fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import testFilms from './js/moviesAPI/test';
 // testFilms();
 import { ifItFirstOupen } from './js/renderMarkupFilmoteka/renderMarkupFilmoteka';
 import { onSearch } from './js/renderMarkupFilmoteka/renderMarkupFilmoteka';
-import { markupUserLogin } from './js/markupUserLogin/markupUserLogin';
+
 import { onFirstOupen } from './js/renderMarkupFilmoteka/renderMarkupFilmoteka';
 import { buttonUp } from './js/scroll/button-up';
 import toggleModalFilm from './js/modals/modal-film';
