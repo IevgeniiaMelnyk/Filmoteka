cbd7918ea8fc658e35784e469119b2bac26ce3aa
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-01-04 20:42:33 +0100

    fix header
    fix header

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ import { retrySearch } from './js/errors/retrySearch';
 import { onPagination } from './js/pagination/pagination';
 import { loginModal } from './js/firebase/loginModal';
 
-
-// import { firebaseAuth } from './js/firebase/fbAuth';
-
+import { firebaseAuth } from './js/firebase/fbAuth';
 
 import { changeMode } from './js/changeMode/changeMode';
--- src/js/userLogin.js
+++ src/js/userLogin.js
@@ -1,26 +1,17 @@
-// import { getRefs } from '../refs';
+import { getRefs } from '../refs';
 
-// function getRefs() {
-//   return {
-//     signOut: document.querySelector('#signout'),
-//     signUp: document.querySelector('#signup'),
-//     signIn: document.querySelector('#signin'),
-//     userIcon: document.querySelector('#usericon'),
-//   };
-// }
+const refs = getRefs();
 
-// const refs = getRefs();
-
-// export function markupUserLogin(isLogin, userName) {
-//   if (isLogin) {
-//     refs.signOut.classList.remove('visually-hidden');
-//     refs.userIcon.classList.remove('visually-hidden');
-//     refs.signUp.classList.add('visually-hidden');
-//     refs.signIn.classList.add('visually-hidden');
-//   } else if (!isLogin) {
-//     refs.signOut.classList.add('visually-hidden');
-//     refs.userIcon.classList.add('visually-hidden');
-//     refs.signUp.classList.remove('visually-hidden');
-//     refs.signIn.classList.remove('visually-hidden');
-//   }
-// }
+export function markupUserLogin(isLogin, userName) {
+  if (isLogin) {
+    refs.signOut.classList.remove('visually-hidden');
+    refs.userIcon.classList.remove('visually-hidden');
+    refs.signUp.classList.add('visually-hidden');
+    refs.signIn.classList.add('visually-hidden');
+  } else if (!isLogin) {
+    refs.signOut.classList.add('visually-hidden');
+    refs.userIcon.classList.add('visually-hidden');
+    refs.signUp.classList.remove('visually-hidden');
+    refs.signIn.classList.remove('visually-hidden');
+  }
+}
