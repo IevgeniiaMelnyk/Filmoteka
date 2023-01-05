8d3c3e35db6ae166da68eb664154b2dedb3d418f
Svitlana Cherniakova <107955029+Svchernn@users.noreply.github.com>
2023-01-05 21:24:57 +0200

    Update loginModal.js

--- src/js/firebaseFilm/loginModal.js
+++ src/js/firebaseFilm/loginModal.js
@@ -36,19 +36,29 @@ const formReg = document.querySelector('.reg-form');
 formLog.addEventListener('submit', logSubmit);
 formReg.addEventListener('submit', regSubmit);
 
+async function exampleLogin() {
+  const email = email.value;
+  const password = password.value;
+  const result = await fbFilmsAuth.login(email, password);
+  console.log(result);
+  console.log(fbFilmsAuth.getUserDisplayName());
+}
+
+async function exampleRegistration() {
+  const email = email.value;
+  const password = password.value;
+  const user = name.value;
+  const result = await fbFilmsAuth.singUp(email, password, user);
+  console.log(result);
+  console.log(fbFilmsAuth.getUserDisplayName());
+}
+
 function logSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, password },
   } = event.currentTarget;
 
-  async function exampleLogin() {
-    const email = email.value;
-    const password = password.value;
-    const result = await fbFilmsAuth.login(email, password);
-    console.log(result);
-    console.log(fbFilmsAuth.getUserDisplayName());
-  }
   event.currentTarget.reset();
 }
 
@@ -58,13 +68,5 @@ function regSubmit(event) {
     elements: { name, email, password },
   } = event.currentTarget;
 
-  async function exampleRegistration() {
-    const email = email.value;
-    const password = password.value;
-    const user = name.value;
-    const result = await fbFilmsAuth.singUp(email, password, user);
-    console.log(result);
-    console.log(fbFilmsAuth.getUserDisplayName());
-  }
   event.currentTarget.reset();
 }
