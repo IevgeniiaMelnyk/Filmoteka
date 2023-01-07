986bf4bac634e19fe8a21af0269100fef2ecabc7
Ievgeniia Melnyk <agnikava@gmail.com>
2023-01-07 21:35:43 +0200

    add fix

--- src/js/renderLogin/isLogin.js
+++ src/js/renderLogin/isLogin.js
@@ -42,11 +42,5 @@ export function showMessage(isLogin) {
   if (isLogin) {
     needRegisterHide();
     emptyLibraryShow();
-    refs.libraryBtnQ.classList.remove('not-active')
-    refs.libraryBtnW.classList.remove('not-active')
-  }
-  if (!isLogin) {
-    refs.libraryBtnQ.classList.add('not-active');
-    refs.libraryBtnW.classList.add('not-active');
-  }
-}
+  };
+};
